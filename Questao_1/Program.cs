5547b4a6174280d9adce4265748a00adc3d3dba8
SamuelLuzSantana <slsamuelluz@gmail.com>
2022-02-16 18:18:35 -0300

    Questão 1 resolvida

---
+++ Questao_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços.
+//A base e altura da escada devem ser iguais ao valor de n. A última linha não deve conter nenhum espaço.
+
+Console.WriteLine("Digite um numero");
+int numero = Convert.ToInt32(Console.ReadLine()) ;
+
+
+string padrao = "";
+
+for (var i = 1; i <= numero; i++)
+{
+    padrao += "*";
+     Console.WriteLine(new String(' ', numero - padrao.Length) + padrao);
+
+}
+
+
